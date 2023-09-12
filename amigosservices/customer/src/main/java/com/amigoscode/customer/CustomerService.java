332a7a82a5881b865794721e2a0f2325410f7279
SStoyanov22 <SStoyanov22@gmail.com>
2023-09-12 19:48:54 +0300

    Adding customer service

---
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package com.amigoscode.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+
+    public void register(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // todo: check if email is valid
+        // todo: check if email is not taken
+        customerRepository.save(customer);
+    }
+}
